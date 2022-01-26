616755293118346cc6329cadb6468f54c8673333
Benjamin Baranger <baranger.benjamin@gmail.com>
2022-01-26 17:52:15 +0000

    fix syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root, to: "schemas#index"
+  root to: "schemas#index"
   resources :schemas
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
